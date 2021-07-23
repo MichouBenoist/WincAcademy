bb05c2fefe9de79cc28a28b26344c4084e6dbe7c
Michou Benoist <a.m.benoist@gmail.com>
2021-07-23 16:06:12 +0200

    Eerste javascript oefeningen

---
+++ hello_world/opdracht_10/script.js
@@ -0,0 +1,27 @@
+console.log('Hello Winc Academy')
+let naam = 'Michou Benoist'
+console.log(naam)
+
+//let if = 'Dit werkt niet he'
+// Deze werkt niet want if is een reserved keyword.
+
+let uitkomst = 4 + 4
+console.log(uitkomst)
+
+let uitkomsttweestrings = "4" + "4"
+console.log(uitkomsttweestrings)
+
+let additionMultiplication = 2 + 2 * 3
+console.log(additionMultiplication)
+
+let subtractionDivison = 3 - 5 / 2
+console.log(subtractionDivison)
+
+let modulus = 5 % 2
+console.log(modulus)
+
+let leeftijd = 37
+console.log(typeof leeftijd)
+
+let leeftijdTekst = '37'
+console.log(typeof leeftijdTekst)
