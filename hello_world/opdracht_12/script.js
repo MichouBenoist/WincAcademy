8c6846585e304c890c2ff1f7b3200f7ff37cad7d
Michou Benoist <a.m.benoist@gmail.com>
2021-07-24 18:28:20 +0200

    If else statements

---
+++ hello_world/opdracht_12/script.js
@@ -0,0 +1,37 @@
+const age = 18;
+
+if (age > 18) {
+
+    console.log("Welkom. Je mag drinken.");
+} else if (age < 18) {
+
+    console.log("Je mag nog niet drinken, ik verzoek je naar buiten te gaan.");
+}
+if (age == 18) {
+    console.log("Gefeliciteerd, je bent net 18. Je mag drinken.");
+}
+
+
+
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log("Welkom dame, het is ladiesnight en je mag gratis naat binnen.");
+}
+else {
+    console.log("Goedenavond meneer, welkom. Dat is dan tien euro entree.");
+}
+
+
+
+
+
+const driverStatus = "bob";
+
+if (driverStatus === "bob") {
+    console.log("Je mag rijden!");
+}
+else {
+    console.log("Zoek je iemand die jou naar huis brengt?");
+}
