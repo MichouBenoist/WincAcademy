6b4ce020d59f725f1236c45e8343a379b667eb81
Michou Benoist <a.m.benoist@gmail.com>
2021-07-24 15:18:20 +0200

    Javascript en comments toevoegen

--- hello_world/opdracht_11/script.js
+++ hello_world/opdracht_11/script.js
@@ -1,2 +1,15 @@
 let groet = "Hallo Winc Academy studenten"
 console.log(groet)
+
+let som = 1230941 * 1823794
+console.log(som)
+
+let groteSom = 637263 / 54
+console.log(groteSom)
+
+// "Dit is een grote som".
+
+/* De comment zie je niet op je console, dat is alleen zichtbaar in VSCode. */
+
+/* uitkomst van de som zie je niet op je console, die is nu comment geworden en niet zichtbaar. */
+
