79fe4dbde6ed0ec798d3e46880962526d6f36ee7
Michou Benoist <a.m.benoist@gmail.com>
2021-08-11 14:33:37 +0200

    function basics opdracjt gemaakt

---
+++ hello_world/opdracht_15/script.js
@@ -0,0 +1,24 @@
+function greet(name) {
+    console.log('Welkom in ons restaurant ' + name + ', wat wil je bestellen?');
+}
+greet('Michou');
+
+makeSushi();
+makeSushi();
+makeSushi();
+
+function makePizza() {
+    console.log('Maak de bodem');
+    console.log('Beleg de pizza');
+    console.log('Bak de pizza');
+}
+
+makePizza();
+makePizza();
+
+
+function makeSushi() {
+    console.log('Maak Sushi')
+}
+
+
